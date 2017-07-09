7a3445310739072d6b1dd9a400fa2b515285b48a
Brian Chan <i@b123400.net>
2017-07-09 06:36:12 +0100

    Update style

--- src/Pux/Form/Builder.purs
+++ src/Pux/Form/Builder.purs
@@ -2,12 +2,9 @@ module Pux.Form.Builder where
 
 import Prelude
 import Data.Exists (Exists, mkExists, runExists)
-import Data.Foldable (foldl)
-import Data.Array (snoc)
 import Data.Int (fromString)
 import Data.Maybe (Maybe(..))
 import Data.Lens (Lens, view, set)
-import Data.Newtype (class Newtype, unwrap)
 import Partial.Unsafe (unsafePartial)
 
 import Pux.DOM.HTML (HTML)
@@ -49,12 +46,20 @@ andField a b =
   runExists (\b'->
     runExists (\a'->
       mkExists $ unsafePartial $ unsafeMerge a' b') a) b
-  where unsafeMerge :: forall x1 x2. Partial => FieldsF s e a1 x1 -> FieldsF s e a2 x2 -> FieldsF s e a2 a1
-        unsafeMerge a (FieldsF lens ren pred _) = FieldsF lens ren pred (mkExists a)
+  where unsafeMerge :: forall x1 x2
+                    .  Partial
+                    => FieldsF s e a1 x1
+                    -> FieldsF s e a2 x2
+                    -> FieldsF s e a2 a1
+        unsafeMerge first (FieldsF lens ren pred _) = FieldsF lens ren pred (mkExists first)
 
 infixl 4 andField as .+
 
-fieldWithDOM :: forall s e a. (Render a) => Lens s s a a -> HTML e -> Fields s e a
+fieldWithDOM :: forall s e a
+             .  (Render a)
+             => Lens s s a a
+             -> HTML e
+             -> Fields s e a
 fieldWithDOM lens dom = mkExists $ FieldsF lens customRender (const true) $ mkExists NoField
   where customRender a b =  dom *> (render a b)
 
@@ -71,24 +76,13 @@ infixl 5 withPred as .?
 form :: forall s e a. Fields s e a -> s -> Builder s e a
 form = flip Builder
 
-renderForm :: forall s e a. Builder s e a -> (s -> e) -> HTML e
+renderForm :: forall s e a1. Builder s e a1 -> (s -> e) -> HTML e
 renderForm (Builder obj f) event = HTML.form inputs
   where inputs = toInputs f
-        toInputs :: forall a. Fields s e a -> HTML e
+        toInputs :: forall a2. Fields s e a2 -> HTML e
         toInputs = runExists (\f'-> case f' of
           NoField -> text ""
           (FieldsF lens ren pred rest) -> toInputs rest *> ren (\a-> if pred a
                                                                      then event $ set lens a obj
                                                                      else event obj)
                                                                (view lens obj))
-
-
--- form :: FormData
--- form =    httpPort .| text "http port"        .? (\x-> x > 1000 && x < 65535)
---        .> destDir  .| text "dest directory"   ./ filePicker
---        .> inline   .| text "Combine JS files"
-
--- runForm :: FormData -> (State -> Event) -> HTML
-
--- instance Fieldable Int where
---   field = input ! (type "number")
