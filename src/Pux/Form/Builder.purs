f7be0b487b6810e6a67d86ec06c96ca9a6e63649
Brian Chan <i@b123400.net>
2017-07-09 06:29:43 +0100

    Use linked list for fields, take field type out from Exists in order to implement pred

--- src/Pux/Form/Builder.purs
+++ src/Pux/Form/Builder.purs
@@ -4,8 +4,11 @@ import Prelude
 import Data.Exists (Exists, mkExists, runExists)
 import Data.Foldable (foldl)
 import Data.Array (snoc)
+import Data.Int (fromString)
+import Data.Maybe (Maybe(..))
 import Data.Lens (Lens, view, set)
 import Data.Newtype (class Newtype, unwrap)
+import Partial.Unsafe (unsafePartial)
 
 import Pux.DOM.HTML (HTML)
 import Pux.DOM.Events (onChange, targetValue)
@@ -13,25 +16,15 @@ import Text.Smolder.HTML as HTML
 import Text.Smolder.HTML.Attributes (value, type')
 import Text.Smolder.Markup (text, (!), (#!))
 
-data Builder s e = Builder s (Fields s e)
+data Builder s e a = Builder s (Fields s e a)
 
-data Fields s e = Fields (Fields s e) (Field s e)
-                | NoField
+data FieldsF s e a a2 = FieldsF (Lens s s a a)
+                                ((a -> e) -> a -> HTML e)
+                                (a -> Boolean)
+                                (Fields s e a2)
+                      | NoField
 
-data FieldF s e a = FieldF (Lens s s a a) ((a -> e) -> a -> HTML e) (a -> Boolean)
-
-newtype Field s e = Field (Exists (FieldF s e))
-
-derive instance newtypeField :: Newtype (Field s e) _
-
-class AsFields a s e where
-  toFields :: a s e -> Fields s e
-
-instance fieldsAsFields :: AsFields Fields s e where
-  toFields = id
-
-instance fieldAsFields :: AsFields Field s e where
-  toFields = Fields NoField
+type Fields s e a = Exists (FieldsF s e a)
 
 class Render a where
   render :: forall e. (a -> e) -> a -> HTML e
@@ -41,32 +34,47 @@ instance renderString :: Render String where
                                 ! (type' "text")
                                 #! onChange (toEvent <<< targetValue)
 
-field :: forall s a e. (Render a) => Lens s s a a -> Field s e
-field lens = Field $ mkExists $ FieldF lens render (const true)
 
-andFields :: forall s e. Fields s e -> Field s e -> Fields s e
-andFields = Fields
+field :: forall s e a. (Render a) => Lens s s a a -> Fields s e a
+field lens = mkExists $ FieldsF lens render (const true) $ mkExists NoField
 
-infixl 4 andFields as .>
-
-andField :: forall s e. Field s e -> Field s e -> Fields s e
-andField = (andFields <<< toFields)
+andField :: forall s e a1 a2. Fields s e a1 -> Fields s e a2 -> Fields s e a2
+andField a b =
+  runExists (\b'->
+    runExists (\a'->
+      mkExists $ unsafePartial $ unsafeMerge a' b') a) b
+  where unsafeMerge :: forall x1 x2. Partial => FieldsF s e a1 x1 -> FieldsF s e a2 x2 -> FieldsF s e a2 a1
+        unsafeMerge a (FieldsF lens ren pred _) = FieldsF lens ren pred (mkExists a)
 
 infixl 4 andField as .+
 
-form :: forall s e a. (AsFields a s e) => a s e -> s -> Builder s e
-form = (flip Builder) <<< toFields
+fieldWithDOM :: forall s e a. (Render a) => Lens s s a a -> HTML e -> Fields s e a
+fieldWithDOM lens dom = mkExists $ FieldsF lens customRender (const true) $ mkExists NoField
+  where customRender a b =  dom *> (render a b)
 
-renderForm :: forall s e. Builder s e -> (s -> e) -> HTML e
-renderForm (Builder obj f) event = HTML.form (foldl (*>) (text "") children)
-  where fields = genFields f
-        children = fields
-                   <#> unwrap
-                   <#> runExists (\(FieldF lens r _)-> r (\a-> event $ set lens a obj) (view lens obj))
+infixl 5 fieldWithDOM as .|
 
-genFields :: forall s e. Fields s e -> Array (Field s e)
-genFields NoField = []
-genFields (Fields a b) = snoc (genFields a) b
+withPred :: forall s a e. Fields s e a -> (a -> Boolean) -> Fields s e a
+withPred fields pred = runExists (\f-> case f of
+    (FieldsF lens ren _ next) -> mkExists $ FieldsF lens ren pred next
+    NoField                   -> mkExists NoField
+  ) fields
+
+infixl 5 withPred as .?
+
+form :: forall s e a. Fields s e a -> s -> Builder s e a
+form = flip Builder
+
+renderForm :: forall s e a. Builder s e a -> (s -> e) -> HTML e
+renderForm (Builder obj f) event = HTML.form inputs
+  where inputs = toInputs f
+        toInputs :: forall a. Fields s e a -> HTML e
+        toInputs = runExists (\f'-> case f' of
+          NoField -> text ""
+          (FieldsF lens ren pred rest) -> toInputs rest *> ren (\a-> if pred a
+                                                                     then event $ set lens a obj
+                                                                     else event obj)
+                                                               (view lens obj))
 
 
 -- form :: FormData
